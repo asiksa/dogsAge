9951cd746caed896bd3c1be1e7502fab95d0e3c6
Joanna Pruszkowska <39525035+asiksa@users.noreply.github.com>
2018-09-18 21:39:46 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+//my age
+let myAge= 30;
+//early years
+let earlyYears = 2; 
+earlyYears = earlyYears * 10.5;
+//later year
+let laterYears = myAge-2;
+lateryears = laterYears * 4;
+console.log(earlyYears);
+console.log(laterYears);
+//my age in dog's years
+let myAgeInDogYears = (earlyYears+laterYears);
+let myName = "Adrian";
+myName = myName.toLowerCase();
+console.log(`My Name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
